de4637ea5941ed7585607cc5f30440b94fb83ec4
Gandino1984 <andinogerman@gmail.com>
2024-05-20 15:58:29 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const CONTAINER_PORT = 3000;
+
+const app = express();
+
+app.get("/", (req,res)=>{
+    res.json({message:"Storypot says hi!"});
+})
+
+app.listen(CONTAINER_PORT, ()=>{
+    console.log("Storypot: app running in port " + process.env.APP_PORT);
+})
