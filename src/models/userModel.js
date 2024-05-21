0a8f5a3c7a566c5b6a549c7feceace162b80e595
Gandino1984 <andinogerman@gmail.com>
2024-05-21 09:47:30 +0200

    user model creation

---
+++ src/models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+
+    email:{
+        type:String,
+        required: true,
+        unique: true
+    },
+
+    username : {
+        type: String,
+        required: true,
+        unique:  true
+    },
+
+    password:{
+        type: String,
+        required:true
+    },
+
+    role:{
+        type: String,
+        enum:["reader", "producer", "editor"],
+        default: "reader"
+    } 
+
+});
+
+const userModel = mongoose.model("users", userSchema);
