11a6067a87ed843d3725f4726ce2682132739319
Gandino1984 <andinogerman@gmail.com>
2024-05-22 10:54:37 +0200

    comment resource controller

--- src/controllers/resourceController.js
+++ src/controllers/resourceController.js
@@ -1,52 +1,99 @@
 import resourceModel from "../models/resourceModel.js";
-import projectModel from "../models/projectModel.js";
 
+// This function retrieves all resources from the database and returns them.
+// If there is an error, it logs the error and returns an empty array.
 const getAll = async()=>{
     try {
+        // This line uses the `find` method of the `resourceModel` to retrieve all documents in the collection.
+        // The `await` keyword is used to wait for the query to complete before moving on to the next line of code.
         const resources = await resourceModel.find();
+        
+        // This line returns the retrieved resources.
         return resources;
     } catch(error){
+        // If there is an error, this line logs the error to the console.
         console.error(error); 
+        
+        // This line returns an empty array.
         return [];
     }
 }
 
+
+
+// This function retrieves a resource from the database by its ID and returns it.
+// If there is an error, it logs the error and returns null.
 const getById = async(id)=>{
     try {
+        // This line uses the `findById` method of the `resourceModel` to retrieve a document by its ID.
+        // The `await` keyword is used to wait for the query to complete before moving on to the next line of code.
         const resource = await resourceModel.findById(id);
+        
+        // This line returns the retrieved resource.
         return resource;
     } catch (error) {
+        // If there is an error, this line logs the error to the console.
         console.error(error);
+        
+        // This line returns null.
         return null;
     }
 }
 
+
+// This function creates a new resource in the database and returns it.
+// If there is an error, it logs the error and returns null.
 const create = async(data) =>{
     try {
+        // This line uses the `create` method of the `resourceModel` to create a new document in the collection.
+        // The `await` keyword is used to wait for the query to complete before moving on to the next line of code.
         const resource = await resourceModel.create(data);
+        
+        // This line returns the newly created resource.
         return resource;
     } catch(error){
+        // If there is an error, this line logs the error to the console.
         console.error(error);
+        
+        // This line returns null.
         return null;
     }
 }
 
-const update = async(id,data)=>{
-    try {
-        const resource = await resourceModel.findByIdAndUpdate(id,data);
-        return resource;
-    } catch (error) {
-        console.error(error);
-        return null;
-    }    
-}
 
+// This function updates an existing resource in the database and returns it.
+// If there is an error, it logs the error and returns null.
+const update = async(id, data) =>{
+    try {
+        // This line uses the `findByIdAndUpdate` method of the `resourceModel` to update a document by its ID.
+        // The `await` keyword is used to wait for the query to complete before moving on to the next line of code.
+        const resource = await resourceModel.findByIdAndUpdate(id, data);
+        
+        // This line returns the updated resource.
+        return resource;
+    } catch(error){
+        // If there is an error, this line logs the error to the console.
+        console.error(error);
+        
+        // This line returns null.
+        return null;
+    }
+}
+// This function removes a resource from the database by its ID and returns the removed resource.
+// If there is an error, it logs the error and returns null.
 const remove = async(id) =>{
     try {
+        // This line uses the `findByIdAndDelete` method of the `resourceModel` to delete a document by its ID.
+        // The `await` keyword is used to wait for the query to complete before moving on to the next line of code.
         const resource = await resourceModel.findByIdAndDelete(id);
+        
+        // This line returns the removed resource.
         return resource;
-    }catch (error) {
+    } catch(error){
+        // If there is an error, this line logs the error to the console.
         console.error(error);
+        
+        // This line returns null.
         return null;
     }
 }
