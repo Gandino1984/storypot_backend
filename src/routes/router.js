f78a271099e7465e310ed3c8fbfb1deb2f9f54b3
Gandino1984 <andinogerman@gmail.com>
2024-05-22 12:34:25 +0200

    userApi

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,11 +1,13 @@
 import {Router} from "express";
 
+import userRouter from "./userRouter.js";
+
 const router = Router();
 
-
 router.get("/", (req, res) => {
-    res.json("Storypo API says hi!");
+    res.json("Storypot API says hi!");
 })
 
+router.use("/users", userRouter);
 
 export default router;
